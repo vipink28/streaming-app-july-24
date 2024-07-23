ff016b286df605eaaa69d36226896cbcb38e80d8
vipink28 <vipinkalyan28@gmail.com>
2024-07-23 17:25:15 +0530

    Navbar design updated.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,20 @@ import { Link } from 'react-router-dom';
 
 function Navbar(props) {
     return (
-        <div className='flex px-4 py-3 items-center'>
-            <div className='max-w-40'>
-                <h4 className='text-lg font-bold'>Streaming App</h4>
+        <nav className='bg-gradient-to-b from-slate-950 to-transparent text-white fixed w-full z-50 transition-colors duration-300'>
+            <div className='px-4 flex items-center gap-4 min-w-'>
+                <div className='py-2'>
+                    <Link to="/" className='text-2xl font-bold'>Streaming App</Link>
+                </div>
+                {/* menu */}
+                <div className='flex items-center'>
+                    <Link className='font-semibold text-white no-underline py-4 px-3 hover:bg-violet-400 transition' to="/">Home</Link>
+                    <Link className='font-semibold text-white no-underline py-4 px-3 hover:bg-violet-400 transition' to="/tv">Tv Shows</Link>
+                    <Link className='font-semibold text-white no-underline py-4 px-3 hover:bg-violet-400 transition' to="/movie">Movies</Link>
+                    <Link className='font-semibold text-white no-underline py-4 px-3 hover:bg-violet-400 transition' to="/browse">Browse By Genre</Link>
+                </div>
             </div>
-            <div className='flex gap-3'>
-                <Link to="/" className='px-2 text-violet-950 font-medium'>Home</Link>
-                <Link to="/tv" className='px-2 text-violet-950 font-medium'>Tv Shows</Link>
-                <Link to="/movie" className='px-2 text-violet-950 font-medium'>Movies</Link>
-                <Link to="/browse" className='px-2 text-violet-950 font-medium'>Browse By Genre</Link>
-            </div>
-        </div>
+        </nav>
     );
 }
 
